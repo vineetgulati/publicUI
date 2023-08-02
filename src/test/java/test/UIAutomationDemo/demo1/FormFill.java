80cc9f3e2574f2480305422b23bb583d5ff3be5d
“Vineet <vineet.gulati.contractor@pepsico.com>
2023-08-02 11:10:06 +0530

    add latest

--- src/test/java/test/UIAutomationDemo/demo1/FormFill.java
+++ src/test/java/test/UIAutomationDemo/demo1/FormFill.java
@@ -1,12 +1,12 @@
 package test.UIAutomationDemo.demo1;
+
+import org.testng.annotations.Test;
+
 import test.UIAutomationDemo.FormData.FormData1;
 
-
-public class FormFill 
-{
-    public static void main( String[] args ) throws InterruptedException
-    {
-        
+public class FormFill {
+    @Test
+    public void testFormFill() throws InterruptedException {
         FormData1 obj = new FormData1();
         obj.setUp();
         obj.formFill();
